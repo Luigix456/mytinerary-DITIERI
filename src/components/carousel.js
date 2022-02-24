fbeb0a7911b53c9b5a60ab4647fe60cf1cce15c4
Luigix456 <ditieriluis@gmail.com>
2022-02-24 14:37:53 -0300

    Corrección de carousel 1

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -16,20 +16,21 @@ const Carousel = () => {
         spaceBetween={30}
         breakpoints={{
           "@0.00": {
-            slidesPerView: 1,
+            slidesPerView: 2,
             spaceBetween: 10,
+
           },
           "@0.75": {
-            slidesPerView: 1,
-            spaceBetween: 10,
+            slidesPerView: 2,
+            spaceBetween: 20,
           },
           "@1.00": {
-            slidesPerView: 1,
-            spaceBetween: 10,
+            slidesPerView: 3,
+            spaceBetween: 40,
           },
           "@1.50": {
-            slidesPerView: 1,
-            spaceBetween: 10,
+            slidesPerView: 4,
+            spaceBetween: 50,
           },
         }}
         autoplay={{
